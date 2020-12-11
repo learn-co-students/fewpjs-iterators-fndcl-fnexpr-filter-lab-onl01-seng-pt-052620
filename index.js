35673699c246d1635e73b680ea7f17964ebb2229
bebofofum <bri.boehm@gmail.com>
2020-12-11 14:16:14 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+
+
+function findMatching(array, str) {
+    return array.filter( name => name.toLowerCase() === str.toLowerCase() )
+} 
+
+function fuzzyMatch(array, str) {
+    return array.filter( name => (
+        name.toLowerCase()[0] === str.toLowerCase()[0] && name.toLowerCase()[1] === str.toLowerCase()[1]
+        ))
+    // return filteredName
+}
+
+function matchName(array, str) {
+    return array.filter( name => ( name.name === str ))
+}
+
