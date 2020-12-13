4dd18647bbc4b967ae9ae6d9502dba0229b9b433
8bitbran <brandonmar97@gmail.com>
2020-12-13 13:50:50 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solution here
+const { stringContaining } = require("expect");
+
+function findMatching(drivers, name) {
+    return drivers.filter(o => o.toLowerCase() === name.toLowerCase());
+}
+
+function fuzzyMatch(drivers, test) {
+    return drivers.filter(o => o.toLowerCase().indexOf(test.toLowerCase()) === 0);
+}
+
+function matchName(drivers, name) {
+    return drivers.filter(o => o.name === name);
+}
