2ce4498910b5f8f11b098be6fabce5df80ba8f7a
wynterwolf <shinyguns@gmail.com>
2020-12-14 19:49:30 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution here
+function findMatching(drivers, string) { 
+    return drivers.filter(find_name => {
+        if (find_name.toLowerCase() === string.toLowerCase()) {
+            return findMatching
+        }
+    })
+}
+
+function fuzzyMatch(drivers, letter) {
+    return drivers.filter(find_name => {
+        if (letter.charAt(0) === find_name.charAt(0)) {
+            return find_name
+        }
+    })
+}
+
+function matchName(drivers, name) {
+    return drivers.filter(function (find_name) {
+        if (find_name.name == name) {
+            return find_name
+        }
+    })
+}
