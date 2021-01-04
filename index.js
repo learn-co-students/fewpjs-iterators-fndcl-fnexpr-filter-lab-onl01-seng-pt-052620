3ab2d5014ab49ba0cf94e7b3b45f15d776322e4f
Anthony_Romagnolo <anthonyroma741@gmail.com>
2021-01-03 20:01:04 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+  
+function findMatching(array, input) {
+return array.filter( possibleMatch =>
+    possibleMatch.toLowerCase() === input.toLowerCase()
+    )
+}
+
+function fuzzyMatch(array, testString) {
+return array.filter( possibleMatch =>
+    possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+    )
+}
+
+function matchName(array, inputName) {
+return array.filter( record => record.name === inputName)
+}
